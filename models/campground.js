04c55f35e07b6448b17ab6dd9a2e5108efc76248
Cristian049 <suiucristian04@yahoo.com>
2025-04-09 16:17:54 +0300

    Image delete/edit

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
+// https://res.cloudinary.com/demhohdfw/image/upload/v1743769732/YelpCamp/lx9qifrms2dqvhbenlao.jpg
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
+ImageSchema.virtual("cardImage").get(function () {
+  return this.url.replace("/upload", "/upload/ar_4:3,c_crop");
+});
 
 const CampgroundSchema = new Schema({
   title: String,
   price: Number,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   description: String,
   location: String,
   author: {
@@ -27,6 +40,11 @@ CampgroundSchema.post("findOneAndDelete", async function (doc) {
         $in: doc.reviews,
       },
     });
+  }
+  if (CampgroundSchema.images) {
+    for (const img of CampgroundSchema.images) {
+      await cloudinary.uploader.destroy(img.filename);
+    }
   }
 });
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,6 +28,7 @@ router
   .put(
     isLoggedIn,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     wrapAsync(campgrounds.updateCampground)
   )
